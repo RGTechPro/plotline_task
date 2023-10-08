a939be709d4950ee5d05d782ea995e61ed6ea458
Rishabh Gupta <73791635+RGTechPro@users.noreply.github.com>
2023-10-08 16:23:36 +0530

    tooltip's arrow also configured for top position

--- lib/src/services/tooltip/tooltip_border.dart
+++ lib/src/services/tooltip/tooltip_border.dart
@@ -2,14 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:plotline_task/src/services/tooltip/tooltip_position.dart';
 
 class TooltipBorder extends ShapeBorder {
-
- const TooltipBorder(
+  const TooltipBorder(
       {this.usePadding = true,
       required this.renderBox,
       required this.tooltipPosition});
 
- final RenderBox renderBox;  
- final bool usePadding;
+  final RenderBox renderBox;
+  final bool usePadding;
   final TooltipPosition tooltipPosition;
   @override
   EdgeInsetsGeometry get dimensions =>
@@ -29,7 +28,15 @@ class TooltipBorder extends ShapeBorder {
     var movedy = size.height + offset.dy;
     double relative1 = -10;
     double relative2 = 10;
+    void setRelativePositionToTop() {
+      movedy = offset.dy;
+      relative1 = 10;
+      relative2 = -10;
+    }
 
+    if (tooltipPosition == TooltipPosition.top) {
+      setRelativePositionToTop();
+    }
     rect = Rect.fromPoints(rect.topLeft, rect.bottomRight - const Offset(0, 0));
     return Path()
       ..addRRect(
