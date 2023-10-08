2250005d307005bdf9f31a649b2534d0cdcf9968
Rishabh Gupta <73791635+RGTechPro@users.noreply.github.com>
2023-10-08 16:38:30 +0530

    made a readjustment func to avoid border oveflows

--- lib/src/services/tooltip/my_tooltip.dart
+++ lib/src/services/tooltip/my_tooltip.dart
@@ -31,25 +31,55 @@ class _MyToolTipState extends State<MyToolTip> {
     final offset = renderBox.localToGlobal(Offset.zero);
 
     leftpos = offset.dx - 125 + size.width / 2;
-
+    rightpos = leftpos + 250;
     toppos = offset.dy + size.height;
     if (widget.tooltipPosition == TooltipPosition.top) {
       setRelativePositionToTop(offset);
     }
+    TooltipPosition newTooltipPosition = readjust(offset, size);
     final entry = OverlayEntry(
       builder: (context) {
-        return Positioned(left: leftpos, top: toppos, child: buildOverlay());
+        return Positioned(
+            left: leftpos,
+            top: toppos,
+            child: buildOverlay(tooltipPosition: newTooltipPosition));
       },
     );
     overlay.insert(entry);
   }
 
   void setRelativePositionToTop(Offset offset) {
-  
-    toppos = offset.dy-35;
+    toppos = offset.dy - 35;
   }
 
-  Widget buildOverlay() {
+  TooltipPosition readjust(Offset offset, Size size) {
+    TooltipPosition newTooltipPosition=TooltipPosition.auto;
+    //readjustment vertically
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+    if (toppos < 35) {
+      toppos = offset.dy + size.height;
+     newTooltipPosition=TooltipPosition.bottom;
+    }
+
+    if (screenHeight - toppos < 35) {
+      toppos = offset.dy - 35;
+    newTooltipPosition= TooltipPosition.top;
+    }
+    ///////
+
+//readjustment horizontally
+    if (rightpos > screenWidth) {
+      leftpos = leftpos - (rightpos - screenWidth);
+    }
+    if (leftpos < 0) {
+      leftpos = 0;
+    }
+    //////
+    return newTooltipPosition;
+  }
+
+  Widget buildOverlay({required TooltipPosition tooltipPosition}) {
     final renderBox = context.findRenderObject() as RenderBox;
     return Container(
       height: 35,
@@ -57,7 +87,7 @@ class _MyToolTipState extends State<MyToolTip> {
       decoration: ShapeDecoration(
         color: Colors.black,
         shape: TooltipBorder(
-            renderBox: renderBox, tooltipPosition: widget.tooltipPosition),
+            renderBox: renderBox, tooltipPosition: tooltipPosition),
         shadows: const [
           BoxShadow(color: Colors.black, blurRadius: 4.0, offset: Offset(2, 2)),
         ],
